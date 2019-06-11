3040e77252f1388f7bf05d8b6e2e722dfc49a6bb
gnouf1 <jeuxforum26@gmail.com>
2019-06-11 03:27:47 +0200

    Starting to translate
    If you want too continue no problem

--- packages/weather/OpenWeatherMap.py
+++ packages/weather/OpenWeatherMap.py
@@ -43,6 +43,7 @@ def load_config(func):
         return func(payload)
     return wrapper_load_config
 
+
 def acquire_weather(func):
     @functools.wraps(func)
     def wrapper_acquire_weather(payload):
@@ -51,16 +52,80 @@ def acquire_weather(func):
                 utils.output("inter", "acquiring", utils.translate("acquiring"))
 
                 payload["city"] = item["sourceText"].title()
-                payload["observation"] = payload["owm"].weather_at_place(payload["city"])
+                try:
+                    payload["observation"] = payload["owm"].weather_at_place(payload["city"])
+                except:
+                    tmp = payload["city"]
+
+                    es = tmp.find(' ')
+                    vi = tmp.find(',')
+                    pt = tmp.find('.')
+
+                    if es != -1:
+                        tmp = tmp[:es]
+                    elif vi != -1:
+                        tmp = tmp[:es]
+                    elif pt != -1:
+                        tmp = tmp[:es]
+                    payload["city"] = tmp
+                    try:
+                        payload["observation"] = payload["owm"].weather_at_place(payload["city"])
+                    except:
+                        return utils.output("end", "not_found", utils.translate("not_found"))
                 payload["wtr"] = payload["observation"].get_weather()
 
                 return func(payload)
+
         return utils.output("end", "request_error", utils.translate("request_error"))
 
     return wrapper_acquire_weather
 
 
+def future(func):
+    @functools.wraps(func)
+    def wrapper_acquire_future_weather(payload):
+        for item in payload["entities"]:
+            if item["entity"] == "date":
+                payload["date"] = item['resolution']['strValue']
+                payload["date"] = payload["date"]+" 12:00+00"
+                #return func(payload)
+            elif item["entity"] == "datetime":
+                payload["date"] = item['resolution']['values'][0]['value']
+                return func(payload)
+            elif item["entity"] == "city":
+                utils.output("inter", "acquiring", utils.translate("acquiring"))
+
+                payload["city"] = item["sourceText"].title()
+                try:
+                    payload["observation"] = payload["owm"].daily_forecast(payload["city"], limit=6)
+                except:
+                    tmp = payload["city"]
+                    es = tmp.find(' ')
+                    vi = tmp.find(',')
+                    pt = tmp.find('.')
+
+                    if es != -1:
+                        tmp = tmp[:es]
+                    elif vi != -1:
+                        tmp = tmp[:es]
+                    elif pt != -1:
+                        tmp = tmp[:es]
+                    payload["city"] = tmp
+
+                    try:
+                        payload["observation"] = payload["owm"].daily_forecast(payload["city"], limit=6)
+                    except:
+                        utils.output("inter", "city", utils.translate("city", {"city": payload["city"]}))
+                        return utils.output("end", "not_found", utils.translate("not_found"))
+                payload["wtr"] = payload["observation"].get_weather_at(payload["date"])
+                return func(payload)
+
+        return utils.output("end", "request_error", utils.translate("request_error"))
+
+    return wrapper_acquire_future_weather
+
 # Methods
+
 
 @load_config
 @acquire_weather
@@ -89,6 +154,35 @@ def current_weather(payload):
         )
     )
 
+
+@load_config
+@future
+def future_weather(payload):
+    """
+    Get the weather few days in future
+    """
+    detailed_status = payload["wtr"].get_detailed_status()
+    temperatures = payload["wtr"].get_temperature(payload["temperature_units"])   # {"temp_max": 10.5, "temp": 9.7, "temp_min": 9.0}
+    humidity = payload["wtr"].get_humidity()
+    #wind = payload["wtr"].get_wind(payload["wind_speed_units"])   # {"speed": 4.6, "deg": 330}
+
+    return utils.output(
+        "end",
+        "future_weather",
+        utils.translate(
+            "future_weather",
+            {
+                "detailed_status": detailed_status.capitalize(),
+                "city": payload["city"],
+                "date": payload["date"],
+                "temperature": temperatures["temp"],
+                "temperature_units": payload["temperature_units"].capitalize(),
+                "humidity": humidity
+            }
+        )
+    )
+
+
 @load_config
 @acquire_weather
 def temperature(payload):
@@ -111,6 +205,7 @@ def temperature(payload):
         )
     )
 
+
 @load_config
 @acquire_weather
 def humidity(payload):
@@ -131,6 +226,7 @@ def humidity(payload):
             }
         )
     )
+
 
 @load_config
 @acquire_weather
@@ -155,6 +251,7 @@ def wind(payload):
         )
     )
 
+
 @load_config
 @acquire_weather
 def sunrise(payload):
@@ -165,6 +262,7 @@ def sunrise(payload):
     dt = payload["wtr"].get_sunrise_time("date")
 
     return utils.output("end", "sunrise", utils.translate("sunrise", {"time": dt.strftime("%H:%M:%S"), "city": payload["city"]}))
+
 
 @load_config
 @acquire_weather
